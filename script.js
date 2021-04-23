2144ba27397333a1bf7cb0bd68e0e601d11de72b
Divyansh <54841396+Divyansh4721@users.noreply.github.com>
2021-04-24 03:51:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function RandomTeam(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+let flag=true;
+let arr = ['All Stars','Amigos','Bannermen','Bosses','Dominators','Elite','Heatwave','Hustle','Legends','Maniacs','Monarchy','Naturals','Outliers','Power','Revolution','Shakedown','Titans','Vikings'];
+function buttonclicked(){
+  if(flag){
+    function removefrom(a){
+      for( let i = 0; i < arr.length; i++){
+        if ( arr[i] === a) {
+          arr.splice(i, 1);
+        }
+      }
+    }
+    if(arr.length%2!=0){
+      arr.push('Bye');
+    }
+    $('#Valo')[0].innerHTML="<div id='table1'><span>Team 1</span><br></div><div id='table2'><span>Team 2</span><br></div>";
+    while(arr.length>1)
+    {
+      console.log(1);
+      let a=RandomTeam(arr);
+      removefrom(a);
+      let b=RandomTeam(arr);
+      removefrom(b);
+      $('#table1').append(a+'<br>');
+      $('#table2').append(b+'<br>');
+    }
+    document.querySelector('#Valo').innerHTML;
+    flag=false;
+  }
+}
+function addname() {
+  if($('#teamnames')[0].value!=""){
+    $('#Valo').append($('#teamnames')[0].value+'<br>');
+    arr.push($('#teamnames')[0].value);
+    $('#teamnames')[0].value="";
+  }
+  else {
+    alert("Enter Team Name");
+  }
+}
+
+
+{
+  for (var i = 0; i < arr.length; i++) {
+    $('#Valo').append(arr[i]+'<br>');
+  }
+  let a=document.querySelector('#button');
+  a.addEventListener('mousedown', function() {
+    a.style.height='40px';
+    a.style.width='14%';
+    a.style.left='75%';
+    a.style.fontSize='17px';
+  });
+  a.addEventListener('mouseup', function() {
+    a.style.height='50px';
+    a.style.width='15%';
+    a.style.left='75%';
+    a.style.fontSize='20px';
+  });
+  let b=document.querySelector('#submit');
+  b.addEventListener('mousedown', function() {
+    b.style.height='4.5%';
+    b.style.width='14%';
+    b.style.left='80%';
+    b.style.fontSize='18px';
+  });
+  b.addEventListener('mouseup', function() {
+    b.style.height='5%';
+    b.style.width='15%';
+    b.style.left='80%';
+    b.style.fontSize='20px';
+  });
+}
